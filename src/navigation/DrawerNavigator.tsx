f7cc1c02fdad1359f8c158080223b18ea8a29a83
Facundo.Lopez <facundo.lopez@climate.com>
2023-07-06 09:25:20 -0300

    change app name, updates to run on mac and add entity page

--- src/navigation/DrawerNavigator.tsx
+++ src/navigation/DrawerNavigator.tsx
@@ -4,12 +4,14 @@ import {ContactStackNavigator} from './StackNavigator';
 import TabNavigator from './TabNavigator';
 import EntitiesListScreen from '../screens/EntitiesListScreen';
 import SideBar from '../layout/SideBar';
+import EntityScreen from '../screens/EntityScreen';
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => (
   // eslint-disable-next-line react/no-unstable-nested-components, react/jsx-props-no-spreading
   <Drawer.Navigator drawerContent={(props) => <SideBar {...props} />}>
+    <Drawer.Screen name="Entity" component={EntityScreen} />
     <Drawer.Screen name="Entities" component={EntitiesListScreen} />
     <Drawer.Screen name="Home" component={TabNavigator} />
     <Drawer.Screen name="Contact" component={ContactStackNavigator} />
