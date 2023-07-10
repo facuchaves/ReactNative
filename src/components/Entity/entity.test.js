2a330d465759e3c0b82ed10ac0f1fdd4d1be23d0
Facundo.Lopez <facundo.lopez@climate.com>
2023-07-10 09:11:24 -0300

    Add jest coverage and simple test

--- src/components/Entity/entity.test.js
+++ src/components/Entity/entity.test.js
@@ -2,5 +2,6 @@
  * This is a simple example of a test in order to know if jest is setup and ready to test.
  */
 test('renders correctly', () => {
-  expect(true).toBeTruthy();
+  const variable = true;
+  expect(variable).toBeTruthy();
 });
